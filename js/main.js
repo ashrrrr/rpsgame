0ff3efb928f6e53e17a852e2eca1c177f3f44c0c
ashrrrr <ash@ashchan.me>
2021-12-06 18:24:11 -0600

    final draft

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 //int variables
 let playerSelection = '';
 let roundCount = 0;
+let playerScore = 0;
+let compScore = 0;
 
 // return rock, paper, or scissors
 function computerPlay () {
@@ -22,8 +24,10 @@ function playRound (playerSelection, computerSelection) {
     if (playerChoice === compChoice) {
         return `Everyone loses, you both picked ${compChoice}!`;
     } else if ((playerChoice === 'rock' && compChoice === 'scissors') || (playerChoice === 'scissors' && compChoice === 'paper') || (playerChoice === 'paper' && compChoice === 'rock')) {
+        playerScore++;
         return `You win, ${playerChoice} beats ${compChoice}!`;
     } else {
+        compScore++;
         return `You lose, ${compChoice} beats ${playerChoice}!`;
     }
 }
@@ -32,10 +36,20 @@ function game () {
     let keepGoing = true;
     while (keepGoing === true) {
         let playerSelection = prompt('Type in your choice!');
-        playRound(playerSelection, computerPlay());
+        console.log(playRound(playerSelection, computerPlay()));
+        console.log(`Current score is Player ${playerScore} : Computer ${compScore}`);
         roundCount++;
         if (roundCount >= 5) {
             keepGoing = false;
+            if (playerScore === compScore) {
+                console.log(`Everyone loses, both scores are tied at ${playerScore}`);
+            } else if (playerScore > compScore) {
+                console.log(`Winner! You won ${playerScore} games against the computer who won ${compScore}.`);
+            } else {
+                console.log(`Loser! You won ${playerScore} games against the computer who won ${compScore}.`);
+            }        
         }
     }
 }
+
+game();
